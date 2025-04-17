70d6fb9bbe94c9ebfd7d4ee329ca225d2309b05c
tomohiro.maeno <>
2025-04-17 21:47:35 +0700

    add: Add an About page and a News list page.

---
+++ src/pages/about.tsx
@@ -0,0 +1,5 @@
+import Hero from '@/components/hero/hero';
+
+export default function About() {
+  return <Hero />;
+}
---
+++ src/pages/news/index.tsx
@@ -0,0 +1,5 @@
+import Hero from '@/components/hero/hero';
+
+export default function News() {
+  return <Hero />;
+}
