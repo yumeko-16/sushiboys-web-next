2f77fa4fb35c6a65d531569f0b1fe3a9c943cb03
tomohiro.maeno <>
2025-04-22 22:39:19 +0700

    change: Change the setting of the lang attribute.

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -4,6 +4,7 @@ export const siteMeta = {
     'SUSHIBOYSオフィシャルウェブサイト。メンバーはFARMHOUSE、サンテナ、DJ兼カメラマン兼マネージャー兼運転手兼スーパーバイザーのneo yosikawa。寿司ボーイズのライブ、ディスコグラフィ、グッズなどの最新情報を掲載しています。',
   siteUrl: 'https://sushiboys350.com',
   siteLocale: 'ja_JP',
+  siteLang: 'ja',
   siteType: 'website',
   siteIcon: '/favicon.ico',
   siteAppleIcon: '/apple-icon.png',
