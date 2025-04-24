221e0b5a67c1b4c4f7f7fa59ea3930602f9a283b
tomohiro.maeno <>
2025-04-24 20:15:33 +0700

    update: Display an eye-catching image.

--- src/pages/news/post001.tsx
+++ src/pages/news/post001.tsx
@@ -1,19 +1,44 @@
+import Image from 'next/image';
+import defaultEyecatch from '@/images/no-image.png';
 import { getPostBySlug } from '@/lib/api';
 import Container from '@/components/container/container';
 import PostHeader from '@/components/postHeader/postHeader';
 
+const DEFAULT_WIDTH = 600;
+const DEFAULT_HEIGHT = 600;
+
 type Props = {
   title: string;
-  eyecatch?: object;
+  eyecatch: {
+    url: string;
+    width: number;
+    height: number;
+  };
   content: string;
   publish?: string;
 };
 
 export default function Post001({ title, eyecatch, content, publish }: Props) {
+  const src = eyecatch?.url ?? defaultEyecatch;
+  const width = eyecatch?.width ?? DEFAULT_WIDTH;
+  const height = eyecatch?.height ?? DEFAULT_HEIGHT;
+
   return (
     <Container>
       <article>
         <PostHeader title={title} publish={publish} />
+
+        <figure>
+          <Image
+            src={src}
+            alt=""
+            layout="responsive"
+            width={width}
+            height={height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
       </article>
     </Container>
   );
