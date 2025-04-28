77c084d6a308063c5a8f1b2a5248fd3d0cd47434
tomohiro.maeno <>
2025-04-28 23:33:11 +0700

    Add meta data to article pages.

--- src/pages/news/post001.tsx
+++ src/pages/news/post001.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import defaultEyecatch from '@/images/no-image.png';
 import { getPostBySlug } from '@/lib/api';
+import { extractText } from '@/lib/extractText';
+import Meta from '@/components/meta/meta';
 import Container from '@/components/container/container';
 import TwoColumn from '@/components/twoColumn/twoColumn';
 import PostHeader from '@/components/postHeader/postHeader';
@@ -20,15 +22,30 @@ type Props = {
   };
   content: string;
   publish?: string;
+  description: string;
 };
 
-export default function Post001({ title, eyecatch, content, publish }: Props) {
+export default function Post001({
+  title,
+  eyecatch,
+  content,
+  publish,
+  description,
+}: Props) {
   const src = eyecatch?.url ?? defaultEyecatch;
   const width = eyecatch?.width ?? DEFAULT_WIDTH;
   const height = eyecatch?.height ?? DEFAULT_HEIGHT;
 
   return (
     <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
       <article>
         <PostHeader title={title} publish={publish} />
 
@@ -63,6 +80,7 @@ export default function Post001({ title, eyecatch, content, publish }: Props) {
 export async function getStaticProps() {
   const slug = 'post001';
   const post = await getPostBySlug(slug);
+  const description = extractText(post.content);
 
   if (!post) {
     return {
@@ -76,6 +94,7 @@ export async function getStaticProps() {
       eyecatch: post.eyecatch,
       content: post.content,
       publish: post.publishDate ?? null,
+      description: description,
     },
   };
 }
