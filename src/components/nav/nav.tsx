7275e32fc762ddeddf73d4a0f42fa4f252277d07
tomohiro.maeno <>
2025-07-05 17:01:13 +0700

    change: Change the navigation to a hamburger menu.

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,22 +1,50 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './nav.module.scss';
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggeleNav = () => {
+    setNavIsOpen((prev) => !prev);
+  };
+
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
+
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>
+          {`
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          `}
+        </style>
+      )}
+
+      <button className={styles.btn} onClick={toggeleNav}>
+        <span className={styles.bar}></span>
+        <span className="sr-only">Menu</span>
+      </button>
+
       <ul className={styles.list}>
         <li>
-          <Link className={styles.link} href="/">
+          <Link className={styles.link} href="/" onClick={closeNav}>
             Home
           </Link>
         </li>
         <li>
-          <Link className={styles.link} href="/about">
+          <Link className={styles.link} href="/about" onClick={closeNav}>
             About
           </Link>
         </li>
         <li>
-          <Link className={styles.link} href="/news">
+          <Link className={styles.link} href="/news" onClick={closeNav}>
             News
           </Link>
         </li>
