fd9563001b89728489eedb05e40680d624f16a06
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-09-11 01:20:26 +0700

    add: Add PostHeader component.

--- src/_components/Date/index.tsx
+++ src/_components/Date/index.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 export default function Date({ date }: Props) {
   return (
-    <span className={styles.date}>
+    <div className={styles.date}>
       <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
       <ConvertDate dateISO={date} />
-    </span>
+    </div>
   );
 }
